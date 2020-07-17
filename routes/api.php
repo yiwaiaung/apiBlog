3b3de4ac2252142841413780f05f7409be4b4612
yiwaiaung <yiwaiaung849@gmail.com>
2020-07-17 11:29:48 +0630

    Api part one

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post("login","AuthController@login");
+Route::post("register","AuthController@register");
+
+Route::group(["middleware"=>"auth.jwt"],function(){
+    Route::get("logout","AuthController@logout");
+    Route::resource("tasks","TaskController");
+});
+
